7ac69252d119a70e415522f5f19b73b4c94dcbc7
mohira <m.ohira@nowall.co.jp>
2016-09-23 02:00:21 +0900

    seedsファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+30.times do
+  House.create(
+    name:    'メゾン' + ForgeryJa(:name).last_name,
+    price:   ForgeryJa(:monetary).popularity_money,
+    address: ForgeryJa(:address).full_address,
+    note:    Faker::Lorem.paragraphs
+  )
+end
