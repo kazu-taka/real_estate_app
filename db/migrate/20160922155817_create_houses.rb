188eb9e8a1f3a2bb2b569778c8b9b0fc4cc1edc0
mohira <m.ohira@nowall.co.jp>
2016-09-23 01:00:04 +0900

    Migrationスクリプト編集(カラムのオプション設定)

--- db/migrate/20160922155817_create_houses.rb
+++ db/migrate/20160922155817_create_houses.rb
@@ -1,9 +1,9 @@
 class CreateHouses < ActiveRecord::Migration
   def change
     create_table :houses do |t|
-      t.string :name
-      t.integer :price
-      t.string :address
+      t.string :name,    null: false, length: { maximum: 100 }
+      t.integer :price,  null: false
+      t.string :address, null: false
       t.text :note
 
       t.timestamps null: false
