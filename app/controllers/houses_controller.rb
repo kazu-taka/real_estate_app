ac983aac86db62673e0b33cee584020f65a6a23a
mohira <m.ohira@nowall.co.jp>
2016-09-23 03:04:28 +0900

    createアクション

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -12,6 +12,9 @@ class HousesController < ApplicationController
   end
 
   def create
+    @house = House.new(house_params)
+    @house.save
+    redirect_to @house
   end
 
   def edit
@@ -21,5 +24,10 @@ class HousesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:name, :price, :address, :note)
   end
 end
