143c51cfe9109a14ecf5c2e087d41f88932907af
mohira <m.ohira@nowall.co.jp>
2016-09-23 18:12:32 +0900

    ダミーデータの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
+20.times do
+  Seller.create(
+    name: ForgeryJa(:name).full_name,
+    email: Faker::Internet.free_email
+  )
+end
+
 100.times do
   House.create(
     name:    'メゾン' + ForgeryJa(:name).last_name,
     price:   ForgeryJa(:monetary).popularity_money,
     address: ForgeryJa(:address).full_address,
-    note:    Faker::Lorem.paragraphs
+    note:    Faker::Lorem.paragraphs,
+    seller_id: rand(1..20)
   )
 end
+
