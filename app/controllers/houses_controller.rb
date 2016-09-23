1e4bc9a692073a48bf87443aa2ce2833a3421112
mohira <m.ohira@nowall.co.jp>
2016-09-23 23:16:59 +0900

    Housesコントローラのアクセス制限

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,4 +1,5 @@
 class HousesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_house, only: [:show, :edit, :update, :destroy]
 
   def index
